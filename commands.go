441a0562baf4d2f46b6c8c82922c234c5a9df632
Michael Crosby <crosby.michael@gmail.com>
2013-09-18 12:15:26 -0700

    Add default port for link
    Add links unit test file

--- commands.go
+++ commands.go
@@ -1117,6 +1117,11 @@ func (cli *DockerCli) CmdLinks(args ...string) error {
 		return err
 	}
 
+	if cmd.NArg() < 1 && !*flAll {
+		cmd.Usage()
+		return nil
+	}
+
 	v := url.Values{}
 	v.Set("name", cmd.Arg(0))
 
